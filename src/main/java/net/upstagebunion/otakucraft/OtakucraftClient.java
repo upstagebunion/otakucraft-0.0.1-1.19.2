1dfb6e1dab195e7dcd223776e20083aa9fdbfd82
Upstagebunion <fco.garcia.solis@gmail.com>
2023-04-12 23:51:12 -0600

    second attempt

---
+++ src/main/java/net/upstagebunion/otakucraft/OtakucraftClient.java
@@ -0,0 +1,11 @@
+package net.upstagebunion.otakucraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OtakucraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
